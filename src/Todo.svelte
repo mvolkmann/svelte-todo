daca2ae269d5c97aa96f8ebf1e193da5e946bee7
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-06-01 11:06:08 -0500

    switched to dispatching custom events

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,18 +1,18 @@
 <script>
-  export let onDeleteTodo;
-  export let onToggleDone;
+  import {createEventDispatcher} from 'svelte';
+  const dispatch = createEventDispatcher();
   export let todo;
 </script>
 
 <style>
-.done-true {
-  color: gray;
-  text-decoration: line-through;
-}
+  .done-true {
+    color: gray;
+    text-decoration: line-through;
+  }
 </style>
 
 <li>
-  <input type="checkbox" checked={todo.done} on:change={onToggleDone} />
+  <input type="checkbox" checked={todo.done} on:change={() => dispatch('toggleDone')} />
   <span class={'done-' + todo.done}>{todo.text}</span>
-  <button on:click={onDeleteTodo}>Delete</button>
+  <button on:click={() => dispatch('delete')}>Delete</button>
 </li>
--- src/TodoList.svelte
+++ src/TodoList.svelte
@@ -13,16 +13,16 @@
   let uncompletedCount = 0;
   $: uncompletedCount = todos.filter(t => !t.done).length;
 
-  function onAddTodo() {
+  function addTodo() {
     todos = todos.concat(createTodo(todoText));
     todoText = '';
   }
 
-  const onArchiveCompleted = () => todos = todos.filter(t => !t.done);
+  const archiveCompleted = () => todos = todos.filter(t => !t.done);
 
-  const onDeleteTodo = todoId => todos = todos.filter(t => t.id !== todoId);
+  const deleteTodo = todoId => todos = todos.filter(t => t.id !== todoId);
 
-  function onToggleDone(todo) {
+  function toggleDone(todo) {
     const {id} = todo;
     todos = todos.map(t =>
       t.id === id ? {...t, done: !t.done} : t
@@ -31,31 +31,31 @@
 </script>
 
 <style>
-body {
-  font-family: sans-serif;
-  padding-left: 10px;
-}
+  body {
+    font-family: sans-serif;
+    padding-left: 10px;
+  }
 
-button {
-  margin-left: 10px;
-}
+  button {
+    margin-left: 10px;
+  }
 
-li {
-  margin-top: 5px;
-}
+  li {
+    margin-top: 5px;
+  }
 
-ul.unstyled {
-  list-style: none;
-  margin-left: 0;
-  padding-left: 0;
-}
+  ul.unstyled {
+    list-style: none;
+    margin-left: 0;
+    padding-left: 0;
+  }
 </style>
 
 <div>
   <h2>To Do List</h2>
   <div>
     {uncompletedCount} of {todos.length} remaining
-    <button on:click={onArchiveCompleted}>Archive Completed</button>
+    <button on:click={archiveCompleted}>Archive Completed</button>
   </div>
   <br />
   <form>
@@ -66,7 +66,7 @@ ul.unstyled {
       placeholder="enter new todo here"
       bind:value={todoText}
     />
-    <button disabled={!todoText} on:click={onAddTodo}>
+    <button disabled={!todoText} on:click={addTodo}>
       Add
     </button>
   </form>
@@ -74,8 +74,8 @@ ul.unstyled {
     {#each todos as todo}
       <Todo
         todo={todo}
-        onDeleteTodo={() => onDeleteTodo(todo.id)}
-        onToggleDone={() => onToggleDone(todo)}
+        on:delete={() => deleteTodo(todo.id)}
+        on:toggleDone={() => toggleDone(todo)}
       />
     {/each}
   </ul>
